95b4dd824e8829d5e4c6a48d77ffe2a218aa2d4b
iampratt <pratyush0705@gmail.com>
2025-06-04 23:04:15 +0530

    feat: responsive design of about section

--- src/screens/About.tsx
+++ src/screens/About.tsx
@@ -8,30 +8,33 @@ import Face from "../components/3d/face";
 function About() {
     const container=useRef(null)
     useGSAP(()=>{
-        gsap.registerPlugin(ScrollTrigger, SplitText);
-        const text = SplitText.create(container.current, { type: "chars" });
-        gsap.from(text.chars, {
-            scrollTrigger: {
-                trigger: container.current,
-                start: "top 90%",
-                end: "top 50%",
-                scrub:true
-            },
-            opacity: 0.05,
-            stagger: 0.2,
-            ease: "none"
-        });
-        gsap.to(container.current, { opacity: 1 });
+        const mm=gsap.matchMedia()
+        mm.add("(min-width: 1024px)", ()=>{
+            gsap.registerPlugin(ScrollTrigger, SplitText);
+            const text = SplitText.create(container.current, { type: "chars" });
+            gsap.from(text.chars, {
+                scrollTrigger: {
+                    trigger: container.current,
+                    start: "top 90%",
+                    end: "top 50%",
+                    scrub:true
+                },
+                opacity: 0.05,
+                stagger: 0.2,
+                ease: "none"
+            });
+            gsap.to(container.current, { opacity: 1 });
+        })
     }, [])
 
   return (
-    <div id='about' className='relative w-full h-screen bg-[#0d0d0d] flex justify-center items-center'>
+    <div id='about' className='relative w-full h-[100dvh] bg-[#0d0d0d] flex justify-center items-center'>
         <div className="absolute h-screen w-full opacity-20">
             <Face />
         </div>
-        <div className='flex flex-col gap-10 justify-center items-start px-50 text-[#b7ab98]'>
-            <div className='font-bold text-[13.9px] tracking-[6.67px] leading-[17.3px]'>ABOUT ME</div>
-            <div ref={container} className='text-[65px] tracking-[-2.67px] leading-[54.7px] font-bold'>
+        <div className='flex flex-col gap-10 justify-center items-start px-10 lg:px-50 text-[#b7ab98]'>
+            <div className='font-bold lg:text-[13.9px] tracking-[6.67px] leading-[17.3px]'>ABOUT ME</div>
+            <div ref={container} className='text-[3em] lg:text-[65px] tracking-[-2.67px] leading-[54.7px] font-bold'>
                 I'm a <span className="text-[#eb5939]">selectively skilled</span> prodcut designer with strong focus on producing high quality & impactful disgital experience.
             </div>
         </div>
