febea76bb22ae2f39202e5253112d14ce7756871
iampratt <pratyush0705@gmail.com>
2025-06-04 23:09:40 +0530

    fix: deplyoment errors

--- src/components/3d/coffee.tsx
+++ src/components/3d/coffee.tsx
@@ -1,35 +1,35 @@
-import {Canvas, useFrame, useLoader} from '@react-three/fiber'
-import { useGLTF } from '@react-three/drei'
-import { useRef } from 'react';
-import { OBJLoader } from 'three/addons/loaders/OBJLoader.js'
-import { DAELoader } from 'three/addons/loaders/DAELoader.js'
-import * as THREE from 'three'
+// import {Canvas, useFrame, useLoader} from '@react-three/fiber'
+// import { useGLTF } from '@react-three/drei'
+// import { useRef } from 'react';
+// import { OBJLoader } from 'three/addons/loaders/OBJLoader.js'
+// import { DAELoader } from 'three/addons/loaders/DAELoader.js'
+// import * as THREE from 'three'
 
-function CoffeeModel() {
-    const modelRef = useRef<THREE.Object3D>(null)
+// function CoffeeModel() {
+//     const modelRef = useRef<THREE.Object3D>(null)
 
-    const materials = useLoader(MTLLoader, 'Coffee cup/obj/coffee cup.mtl')
-    const obj = useLoader(OBJLoader, 'Coffee cup/obj/coffee cup.obj', (loader) => {
-    materials.preload()
-    loader.setMaterials(materials)
-  })
+//     const materials = useLoader(MTLLoader, 'Coffee cup/obj/coffee cup.mtl')
+//     const obj = useLoader(OBJLoader, 'Coffee cup/obj/coffee cup.obj', (loader) => {
+//     materials.preload()
+//     loader.setMaterials(materials)
+//   })
 
-    useFrame(({ clock }) => {
-        if (!modelRef.current) return;
-        modelRef.current.rotation.y = clock.elapsedTime*0.5
-    })
+//     useFrame(({ clock }) => {
+//         if (!modelRef.current) return;
+//         modelRef.current.rotation.y = clock.elapsedTime*0.5
+//     })
 
-    return <primitive object={obj} ref={modelRef} scale={1} />
-}
+//     return <primitive object={obj} ref={modelRef} scale={1} />
+// }
 
-function Coffee() {
-    return (
-        <Canvas camera={{ position: [0, -2, 10] }}>
-            <ambientLight />
-            <directionalLight position={[3, 3, 0]} />
-            <CoffeeModel />
-        </Canvas>
-    )
-}
+// function Coffee() {
+//     return (
+//         <Canvas camera={{ position: [0, -2, 10] }}>
+//             <ambientLight />
+//             <directionalLight position={[3, 3, 0]} />
+//             <CoffeeModel />
+//         </Canvas>
+//     )
+// }
 
-export default Coffee
+// export default Coffee
